8a3c30747a3e518d1d86b33929bc4b3623723fab
raselcse33 <raselcse330@gmail.com>
2021-12-06 00:00:23 +0600

    about page

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{title}} Page</p>
+  </div>
+</template>
+<script>
+export default {
+  asyncData() {
+    return{
+      title:'About'
+    }
+  },
+  head() {
+    return{
+      title:'about-page',
+      script:[
+        {
+          src:'/about.js'
+        }
+      ],
+      meta:[
+        {
+          hid:'description',
+          name:'description',
+          content:'this is about page'
+        }
+      ]
+    }
+
+  },
+}
+</script>
